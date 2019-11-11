a4e077a8f27b36545421b103609ebf802dc929d8
imaio <inesmaio4@gmail.com>
2019-11-11 12:13:17 +0000

    convert to binary code

---
+++ convertToBinary.js
@@ -0,0 +1,29 @@
+function gap(num){
+    let binary = [];
+    let dividend = num;
+    let remainder = 0;
+    let binaryLenArr = [];
+    let binaryArr = [];
+    let binLen = 0;
+    while (dividend >= 2) {
+        remainder = dividend % 2;
+        binary.push(remainder);
+        dividend = (dividend - remainder) / 2;
+    }
+    binary.push(dividend);
+    binary.reverse();
+    binLen = binary.length - 1;
+    while (binary[binLen] === 0) {
+        binary.pop();
+        binLen--;
+    } 
+    binary = binary.join("");
+    binary = binary.split("1");
+    
+    for (var i = 0; i < binary.length; i++){
+        binaryLenArr.push(binary[i].length)
+    }
+    return Math.max(...binaryLenArr);
+ }
+
+console.log(gap(3000))
