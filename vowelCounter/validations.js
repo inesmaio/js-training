3cd6cb9d325a3712f6bcaefb008b0654f7f2c1f7
imaio <inesmaio4@gmail.com>
2019-11-18 16:29:44 +0000

    #5 - Delete Console.log
    Deleted some console.log left behind for debug purpose.

--- vowelCounter/validations.js
+++ vowelCounter/validations.js
@@ -4,11 +4,9 @@ const inputVal = (input) => {
   const upperRegex = /[A-Z]/g;
   const numberRegex = /[0-9]/g;
   if (input.match(upperRegex)) {
-    console.log(input.match(upperRegex));
     throw new InputError('Lowercase characters only');
   }
   if (input.match(numberRegex)) {
-    console.log(input.match(numberRegex));
     throw new InputError('Characters only');
   }
 };
